8292fe55821ba10d1cd48dc2398fe6862dd483ca
Yasuaki Tamura <y-tamura>
2018-10-02 12:26:11 +0900

    set array to datasoucer

--- SimpleTableViewController/CustomTableViewController.swift
+++ SimpleTableViewController/CustomTableViewController.swift
@@ -8,6 +8,8 @@ import UIKit
 
 class CustomTableViewController: UITableViewController {
 
+    let items = ["1", "2", "3", "4", "5"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,23 +24,22 @@ class CustomTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = items[indexPath.row]
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
